658fd24f2cff6f15f9882cf1aebf551ff55e35c2
LukeMcNemee <nemecl@seznam.cz>
2015-02-02 11:38:13 +0100

    ord, chr, text

---
+++ 13/text.pas
@@ -0,0 +1,38 @@
+{################################################}
+{#                Program text                  #}
+{#        |     |  _  |V| _|\| _ __  _  _       #}
+{#        |__|_||<(/_ | |(_| |(/_|||(/_(/_	#}
+{#                    2015                      #}
+{################################################}
+{# Program pro demonstraci použití funkcí chr a #}
+{# ord pro práci s textovým řetězcem.           #}
+{################################################}
+
+program text;
+
+uses crt;
+
+var 
+  vstup : String;
+  i : integer;
+
+begin
+  writeln('zadejte text');
+  readln(vstup);
+  for i:=1 to length(vstup) do
+  begin
+    write(ord(vstup[i]), ' ' );
+  end;
+  
+  writeln;
+  
+  for i:=33 to 126 do
+  begin
+    write(chr(i), ' ' );
+  end;
+  
+  writeln;
+  readln; 
+end.
+
+  
